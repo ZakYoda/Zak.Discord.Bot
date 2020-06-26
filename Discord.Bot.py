6f00689aa7b22378adfa9ff07bd78460e87525cf
ZakYoda <zakyeomanson@gmail.com>
2020-06-26 03:46:05 +0100

    added the add_rep functionality

--- Discord.Bot.py
+++ Discord.Bot.py
@@ -1,8 +1,10 @@
 from discord.ext import commands
+import discord
+from json import dump, load
 
 
 client = commands.Bot(command_prefix='.')
-MEMBERS_FILENAME = "members.json"
+MEMBERS_FILENAME = "users.json"
 TOKEN_FILENAME = "token.txt"
 OWNER_ID = 104265011714592768  # ID for my Discord, zak#6030
 
@@ -36,11 +38,33 @@ async def on_message(message):
     await client.process_commands(message)
 
 
-@client.command(name="daily")
-# @commands.cooldown(1, 60*60*24, commands.BucketType.user)
-async def on_daily_reward(ctx):
-    # TODO
-    pass
+@client.command(name="rep")
+@commands.cooldown(1, 60*60*24, commands.BucketType.user)
+async def add_rep(ctx, user: discord.User):
+    rep = 0
+    with open(MEMBERS_FILENAME) as f:
+        users = load(f)
+
+    for i in range(len(users)):
+        if user.id == users[i]["id"]:  # User Match
+            users[i]["rep"] += 1  # Add the rep and store it
+            rep = users[i]["rep"]
+            break  # Now we may break the loop for efficiency purposes
+
+    # Now write our changes back to the file
+    with open(MEMBERS_FILENAME, "w", encoding="utf-8") as f:
+        dump(users, f, ensure_ascii=False, indent=4)
+
+    await ctx.send("Given rep to: {}! This user now has **{}** reputation.".format(user.mention, rep))
+
+
+@add_rep.error
+async def add_rep_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        msg = 'This command is only available once a day, please try again in **{:.2f}s**'.format(error.retry_after)
+        await ctx.send(msg)
+    else:
+        raise error
 
 
 if __name__ == "__main__":
