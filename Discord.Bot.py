6ef35901cf9f244db9931b20013a00c57cd3c419
ZakYoda <zakyeomanson@gmail.com>
2020-08-19 14:55:42 +0100

    Adding some extra commands so the bot can be flexible and suitable for multiple servers (even though the bot will most likely only reside in one server). Still needs finishing, though.

--- Discord.Bot.py
+++ Discord.Bot.py
@@ -7,8 +7,8 @@ via Python.
 
 from discord.ext import commands
 import discord
-from json import dump, load
-import time
+from json import dump
+from emoji import emojize
 
 # Here I set up the bot and create a bunch of constants that need tidying up in the future. It works for the moment,
 # But it's very messy and I hate it as of now.
@@ -76,12 +76,8 @@ async def on_guild_join(guild):
 
     data = {"server_name": guild.name,
             "server_id": guild.id,
-            "members": []}
-
-    for guild_member in guild.members:
-        data["members"].append({"name": str(guild_member),
-                                "id": guild_member.id,
-                                "rep": 0})
+            "roles_max": 0,
+            "reaction_roles": []}
 
     with open("{}.json".format(guild.id), "w+", encoding="utf-8") as f:
         dump(data, f, ensure_ascii=False, indent=4)
@@ -159,40 +155,31 @@ async def on_message(message):
     await client.process_commands(message)
 
 
-@client.command(name="rep")
-@commands.cooldown(1, 60 * 60, commands.BucketType.user)
-async def add_rep(ctx, user: discord.User):  # TODO Probably going to disable the reputation system initially.
-    rep = 0
+@client.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def add_reaction_role_help(ctx):
+    await ctx.channel.send("""To add reaction roles, the format is:
+        `.add_reaction_role {message ID} {role name} {emoji name}`
+        For example:
+        `.add_reaction_role [656201333698854932] [YouTuber] [red_square]`""")
 
-    if user.id == ctx.author.id:
-        await ctx.send("You cannot give yourself rep!")
-        await add_rep.reset_cooldown(ctx)  # TODO FIX THIS WEIRD ERROR?
+
+# TODO Finish this command. Need to add a check for the emoji_name parameter using
+# https://pypi.org/project/emoji/
+@client.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def add_reaction_roles(ctx, msg_id=None, role_name=None, emoji_name=None):
+    if not msg_id or not role_name or not emoji_name:
+        await ctx.channel.send("Error, please provide 3 arguments. See help via add_reaction_role_help")
         return
 
-    with open("{}.json".format(ctx.guild.id), encoding="utf-8") as f:
-        data = load(f)
+    for role in ctx.guild.roles:
+        if role.name == role_name:
+            await ctx.channel.send("Error, could not find the Role! Make sure spelling and capitalisation are "
+                                   "correct!")
+            return
 
-    for i in range(len(data["members"])):
-        if user.id == data["members"][i]["id"]:  # User Match
-            data["members"][i]["rep"] += 1  # Add the rep and store it
-            rep = data["members"][i]["rep"]
-            break  # Now we may break the loop for efficiency purposes
-
-    # Now write our changes back to the file
-    with open("{}.json".format(ctx.guild.id), "w", encoding="utf-8") as f:
-        dump(data, f, ensure_ascii=False, indent=4)
-
-    await ctx.send("Given rep to: {}! This user now has **{}** reputation.".format(user.mention, rep))
-
-
-@add_rep.error
-async def add_rep_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        retry_time = time.strftime(r"%H:%M:%S", time.gmtime(error.retry_after))
-        msg = 'This command is only available once a day, please try again in **{}**'.format(retry_time)
-        await ctx.send(msg)
-    else:
-        raise error
+    await ctx.channel.send("{} {} {}".format(msg_id, role_name, emoji_name))  # This is a temporary return...
 
 
 if __name__ == "__main__":
